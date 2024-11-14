b18563187f89f9fa3f371c5a487e9d2ee8965e4e
Matthew <matthew41305@gmail.com>
2024-11-13 21:50:57 -0800

    custom alert that prevents normal navigation to page when link is clicked, and instead produces a custom message "No distractions.."

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function showAlertOnce() {
 }
 alertButton.addEventListener("click", showAlertOnce);
 
+// custom alert
+const favoriteLink = document.getElementById("favoriteLink");
+favoriteLink.addEventListener("click", function(event) {
+    // stops normal nav - wont allow you to open the webite and displays custom message instead
+    event.preventDefault();
+    // custom alert message
+    alert("No distractions! I’m coding!");
+});
